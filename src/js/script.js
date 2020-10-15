7195db95821988f74d2b10c2d59677f892c5d57a
mattskeoch <matt@Matt.localdomain>
2020-10-15 18:01:06 +0800

    Refacotr SCSS & NJK files

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,19 @@ navToggle.addEventListener("click", function () {
     this.setAttribute("aria-expanded", "true");
   }
 });
+
+let toggle = document.querySelector('.nav__link--toggle')
+
+if (localStorage.getItem('dark-mode')){
+  document.body.classList.add('dark-mode'); 
+}
+
+toggle.addEventListener('click', function () {
+  if (document.body.classList.contains('dark-mode')) {   
+    document.body.classList.remove('dark-mode');
+    localStorage.removeItem('dark-mode');
+  } else {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('dark-mode', 'true')
+  }
+});
